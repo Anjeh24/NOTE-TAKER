141b549b980eec176e3ff5911ca45908cbb6139b
Anjeh Mbah <junez24@yahoo.com>
2021-08-22 10:43:53 -0400

    installed more dependencies and redone server

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const data = fs.readFileSync("./db/db.json", "utf-8");
+const noteList = JSON.parse(data);
+
+//creating universal unique identifier for notes (uuid)
+const { v4: uuidv4 } = require('uuid');
 //Setting up the Express App
 
 const app = express();
@@ -12,63 +17,53 @@ const PORT = process.env || 3000;
 // Setting up the Express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, "Develop/public"));
 
-//File routes
-require('./routes/routes')(app);
-
-
-//Setting up routes that sends the user first to AJAX page
-
+//File routes for css and js to load
+//this is for css
+app.get("/assets/css/styles.css", (req, res) => {
+  res.set("content-type", "text/css");
+  res.sendFile(path.join(__dirname, "/assets/css/styles.css"));
+});
+//this is for js
+app.get("/assets/js/index.js", (req, res) =>
+  res.sendFile(path.join(__dirname, "/assets/js/index.js"))
+);
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-
-
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get("/api/notes", (req, res) => {
+    res.set("content-type", "application/json");
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-//Should display api notes get route
 
-app.get('/api/notes', (req, res) => res.json(notes));
 
-//API post routes. Should post new notes
-
-app.post('/api/notes', (req, res) => {
-    const newNotes = req.body;
-  
-    console.log(newNotes);
-  
-    notes.push(newNotes);
-  
-    res.json(newNotes);
-
-    upgradeDb();
-
-    return console.log("Added" + newNotes.title);
-  });
-
-  //To delete an entry / specific note
-
-  app.delete("/api/notes/", => (req, res) {
-    notes.splice(req.params)
-    upgradeDb();
-    console.log("Deleted"+req.params.id);
-  });
-
-//Should keep the json file up to date whenever a note is added or deleted (That is what I am trying to do here.)
-
-const upgradeDb = (filePath, res) => {
-    return fs.writeFile(`${__dirname}${filePath}`, (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  };
-
-//creating the server and assigning it to a variable called "app". Trying to emulate what was in week 11 package. Trying to make things work.
-
-  const app = http.createServer(handleRequest);
-
-//Starts server to begin listening
+///Funtion for writing notes to db
+function noteWriter(notes) {
+  fs.writeFile(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(notes),
+    (err) => (err ? console.err(err) : console.log("note saved to db"))
+  );
+};
+// will assign universal unique identifier to notes written
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  const noteID = uuidv4();
+  note.id = noteID;
+  noteList.push(note);
+  noteWriter(noteList);
+  res.send(note);
+});
+//delete notes upon request
+app.delete("/api/notes/:uuid", (req, res) =>{
+  for(let i = 0; i < noteList.length; i++){
+    if (noteList[i].id === req.params.uuid){
+      noteList.splice(i, 1);
+    } 
+  }
+  noteWriter(noteList);
+  res.end();
+});
+//listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
-
